88819ed955b2426ba9e21937b0250e7f905f425c
vagrant <svinopterix@gmail.com>
2022-10-22 14:56:46 +0000

    Terraform 7.2 added output.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,10 +33,3 @@ resource "yandex_vpc_subnet" "subnet-1" {
   v4_cidr_blocks = ["192.168.10.0/24"]
 }
 
-output "internal_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
-}
-
-output "external_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
-}
