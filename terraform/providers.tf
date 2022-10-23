4ae93a0a53a59c47210718b508e7359bb7f92389
Viacheslav Medvedev <svinopterix@gmail.com>
2022-10-23 11:21:14 +0300

    Merge branch 'main' of https://github.com/svinopterix/devops20

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,18 +5,6 @@ terraform {
     }
   }
   required_version = ">= 0.13"
-
-  backend "s3" {
-    endpoint   = "storage.yandexcloud.net"
-    bucket     = "netology-terraform-backend"
-    region     = "ru-central1"
-    key        = "terraform/terraform.tfstate"
-    access_key = "ajepulao68fvv326a3lu"
-    secret_key = "YCMQq0bb2NXRnasLTrxtXG54MmJ-EfF29EV71oRB"
-
-    skip_region_validation      = true
-    skip_credentials_validation = true
-  }  
 }
 
 provider "yandex" {
