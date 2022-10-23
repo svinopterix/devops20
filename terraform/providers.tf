92885077294bf7881138555c793b04ae6b0d3681
vagrant <svinopterix@gmail.com>
2022-10-23 09:45:24 +0000

    s3 final setup

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -12,9 +12,6 @@ terraform {
     region     = "ru-central1"
     key        = "terraform/terraform.tfstate"
 
-    access_key = local.s3_access_key 
-    secret_key = local.s3_secret_key
-
     skip_region_validation      = true
     skip_credentials_validation = true
   }  
--- terraform/variables.tf
+++
@@ -1,4 +0,0 @@
-locals {
-    s3_access_key = "${var.s3_access_key}"
-    s3_secret_key = "${var.s3_secret_key}"
-}
