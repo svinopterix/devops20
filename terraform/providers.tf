cef495c5c7ec88850dace7e40ef6e1439b2fab7f
Viacheslav Medvedev <svinopterix@gmail.com>
2022-10-23 11:24:50 +0300

    added s3 backend

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,6 +5,16 @@ terraform {
     }
   }
   required_version = ">= 0.13"
+
+  backend "s3" {
+    endpoint   = "storage.yandexcloud.net"
+    bucket     = "netology-terraform-backend"
+    region     = "ru-central1"
+    key        = "terraform/terraform.tfstate"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }  
 }
 
 provider "yandex" {
