321c8303dbab7ff868390d41082301638c3fb1f4
Viacheslav Medvedev <svinopterix@gmail.com>
2022-10-23 12:02:19 +0300

    added s3 key vars

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -12,6 +12,9 @@ terraform {
     region     = "ru-central1"
     key        = "terraform/terraform.tfstate"
 
+    access_key = var.s3_access_key 
+    secret_key = var.s3_secret_key
+
     skip_region_validation      = true
     skip_credentials_validation = true
   }  
